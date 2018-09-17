36e29aedcdfa9bbb4fda1d85dce0d7cbb049b78c
Margrethe <margrethethorsen@gmail.com>
2018-09-17 09:59:52 +0200

    add member

--- github_team.tf
+++ github_team.tf
@@ -21,4 +21,8 @@ resource "github_team_membership" "minimarker" {
   role     = "member"
 }
 
-
+resource "github_team_membership" "kaffegrut" {
+  team_id  = "${github_team.theclass.id}"
+  username = "kaffegrut"
+  role     = "member"
+}
